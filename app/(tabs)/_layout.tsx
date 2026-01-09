4702648ee0a5775f01c47c31fff9969267a5c8e4
katGhost <katghost124@gmail.com>
2026-01-09 19:05:04 +0200

    modification: Hello from Expo

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,33 +1,37 @@
+import { FontAwesome5 } from '@expo/vector-icons';
 import { Tabs } from 'expo-router';
 import React from 'react';
 import { useColorScheme as rnUseColorScheme } from 'react-native';
+import HapticTab from '../../src/components/HapticTab';
 
 
 export default function TabLayout() {
   const colorScheme = rnUseColorScheme();
   const Colors = {
-    light: { tint: '#1f6f8b' },
-    dark: { tint: '#ffffff' },
-  };
-
+    light: {tint: 'dodgerblue'},
+    dark: {tint: 'skyblue'}
+  }
+  
 
   return (
     <Tabs
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
+        tabBarButton: HapticTab,
       }}>
       <Tabs.Screen
         name="index"
         options={{
           title: 'Home',
+          tabBarIcon: ({ color }) => <FontAwesome5 name="home" size={24} color={color} />,
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
           title: 'Explore',
-          //tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          tabBarIcon: ({ color }) => <FontAwesome5 name="file-invoice-dollar" size={24} color={color} />,
         }}
       />
     </Tabs>
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,13 +2,16 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function HomeScreen() {
   return (
-    <View style={styles.titleContainer}>
+    <View style={styles.container}>
       <Text>Hello from Expo!</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    marginTop: 25,
+  },
   titleContainer: {
     flexDirection: 'row',
     alignItems: 'center',
